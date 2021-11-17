01728f744c73511dad264f5154709dbabd0a784c
janghoonPark <you@example.com>
2021-11-18 01:47:04 +0900

    rest init

--- src/main/java/kr/co/tjoeun/security/SecurityConfig.java
+++ src/main/java/kr/co/tjoeun/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package kr.co.tjoeun.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,16 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-	@Autowired
-	private UserDetailsService memberService;
-	
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(memberService);
-	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -31,7 +25,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
 
-	@Bean 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/","/join").permitAll();
+	}
+
+	@Bean
 	public PasswordEncoder passWordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
--- src/main/java/kr/co/tjoeun/service/MemberServiceImpl.java
+++ src/main/java/kr/co/tjoeun/service/MemberServiceImpl.java
@@ -3,16 +3,13 @@ package kr.co.tjoeun.service;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import kr.co.tjoeun.controller.dto.JoinDto;
 import kr.co.tjoeun.model.Member;
 import kr.co.tjoeun.model.MemberContext;
 import kr.co.tjoeun.repository.MemberInfoRepository;
@@ -29,7 +26,7 @@ public class MemberServiceImpl implements UserDetailsService {
 		Member member = memberInfoRepository.findByUserId(userId);
 		if (member == null)
 			throw new UserNotFoundException("유저가 존재하지 않습니다.");
-		return new MemberContext(member, addAuthorities(member));
+		return new MemberContext(member, addAuthorities(member), member);
 	}
 
 	private Set<GrantedAuthority> addAuthorities(Member member) {
